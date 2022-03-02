ec35235e18514bef9c730185e321b28a4220190d
milo <milobluebell@gmail.com>
2022-03-03 02:55:46 +0800

    feat(WIP): prepared to write generateBundle hooks.

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
-import Vinyl from 'vinyl'
-import utils from '@rollup/pluginutils'
 
-interface IPluginProps {
+import chalk from 'chalk';
+import { NormalizedOutputOptions, OutputBundle, EmittedAsset } from 'rollup';
+import { chalkSay } from './utils'
+export interface IPluginProps {
   // 配置那些资源需要内联
-  inlineSource: string | RegExp
+  include?: string[] | RegExp;
   // 是否压缩
   compress?: boolean;
+  // 遇到哪些属性就使此插件忽略内联请求
+  // 比如 ['async', 'defer', { key: 'type', value: 'module' }, { key: 'rel', value: 'modulepreload' }, { key: 'href', value: '//at.alicdn.com/t/font_2857489_imieufzfwmb.css'}]
+  excludeAttrs?: Array<{ key: string, value: string } | string>;
+  exclude?: RegExp;
   // 项目根目录
   root?: string;
   // 是否允许从远程获取资源？
@@ -13,12 +18,68 @@ interface IPluginProps {
   download?: boolean;
   // 没有找到资源的解决策略
   // 2代表error；1代表warn；0代表none
-  nonMatched: 2 | 1 | 0 | 'error' | 'warn' | 'none'
+  nonMatched?: 2 | 1 | 0 | 'error' | 'warn' | 'none';
 }
 
-export default function (options: IPluginProps) {
+const DEFAULT_CONFIGS = {
+  include: /\.(js|css)$/,
+  compress: true,
+  excludeAttrs: ['async', 'defer', { key: 'type', value: 'module' }, { key: 'rel', value: 'modulepreload' }],
+  exclude: /\.(svg|jpg)$/,
+  download: true,
+  nonMatched: 2
+}
+
+export default function inlineResource(configs: IPluginProps) {
+  const { include, compress = DEFAULT_CONFIGS.compress, excludeAttrs = DEFAULT_CONFIGS.excludeAttrs, exclude = DEFAULT_CONFIGS.exclude, download = DEFAULT_CONFIGS.download, nonMatched = DEFAULT_CONFIGS.nonMatched } = configs;
   return {
     name: 'rollup-plugin-inline-source',
-    
+    // @ts-ignore
+    async generateBundle: (output: NormalizedOutputOptions, bundles: OutputBundle) => {
+      chalkSay('here were generateBundle content')
+      console.log(Object.values(bundles));
+      chalkSay('above were generateBundle\'s content')
+      console.log(Object.keys(bundles));
+      chalkSay('Hey, here we test', chalk.bgYellow.blue);
+
+
+      /**
+       * Main
+       */
+      // const scanFiles = ['index.html'];
+      // const terserOption = {
+      //   removeComments: true,
+      //   collapseWhitespace: true,
+      //   removeEmptyAttributes: true,
+      // };
+      // //
+      // const scannedFileContent = getLocalFileSourceContent(scanFiles[0]);
+      // const scriptNodeStrings = scannedFileContent.match(/\<script.+\>\<\/script\>/g);
+      // const linkNodeStrings = scannedFileContent.match(/\<link.+\/?>/g);
+      // const needMountNodes = [...scriptNodeStrings, ...linkNodeStrings].filter(function (nodeContent) {
+      //   return checkShouldMount(nodeContent, ['async', 'defer', { key: 'rel', value: 'modulepreload' }]);
+      // });
+      // let result = scannedFileContent;
+      // console.log('----- COMMON.baseUrl is: ' + COMMON.baseUrl);
+      // console.log('Mounting resources are: ');
+      // console.table(needMountNodes);
+      // needMountNodes.forEach(async (nodeString) => {
+      //   const absPath = path.join(COMMON.distDir, extractTargetPath(nodeString));
+      //   if (fs.existsSync(absPath) || fs.existsSync(absPath.replace(COMMON.baseUrl, ''))) {
+      //     new RegExp(nodeString, 'g').test(result) && console.log('⛵ Located ' + nodeString, ', whoes resource at ' + absPath);
+      //     // 在dist里
+      //     result = result.replace(new RegExp(nodeString, 'g'), `<${extractTagName(nodeString)} ${extractProperties(nodeString).join(' ')}>${fs.readFileSync(absPath)}</${extractTagName(nodeString)}>`);
+      //     console.log('🚗 Mounting ' + nodeString, ', to inline resource, with properties settled as: ' + extractProperties(nodeString).join(' and '));
+      //   } else {
+      //     // 是远程的
+      //     new RegExp(nodeString, 'g').test(result) && console.log('⛵ Remote requested ' + nodeString, 'at URL: ' + extractTargetPath(nodeString));
+      //     const requestedContent = await getRemoteSourceContent(extractTargetPath(nodeString));
+      //     const content = await minify(requestedContent, terserOption);
+      //     result = result.replace(new RegExp(nodeString, 'g'), `<${extractTagName(nodeString)} ${extractProperties(nodeString).join(' ')}>${content}</${extractTagName(nodeString)}>`);
+      //     console.log('🚗 Mounting ' + nodeString, ', to inline resource, with properties settled as: ' + extractProperties(nodeString).join(' and '));
+      //   }
+      // });
+
+    }
   }
 }
--- utils.ts
+++ utils.ts
@@ -2,22 +2,24 @@
 import path from 'path';
 import fs from 'fs';
 import request from 'request';
-import { minify } from 'html-minifier-terser';
+import chalk from 'chalk';
+import { IPluginProps } from '.';
 
 const COMMON = {
+  rootPath: path.resolve(process.cwd(), '.'),
   distDir: path.resolve(process.cwd(), './dist'),
   baseUrl: `https://${process.env.Tencent_COS_Bucket}.file.myqcloud.com`,
 };
 
 
 /**
- * 
+ *
  * @param url remote url address
  * @returns Promise<unknown>
- * 
- * 'curl' a remote url, and resolved content out
+ *
+ * 'curl' a remote url, and resolved contenFt out
  */
-function getRemoteSourceContent(url: URL['href']) {
+export function getRemoteSourceContent(url: URL['href']) {
   const $url = /^http(s)?\/\/.+/.test(url) ? url : `https:${url}`;
   return new Promise((rsl, rej) => {
     request($url, function (err, response, body) {
@@ -32,13 +34,13 @@ function getRemoteSourceContent(url: URL['href']) {
 
 
 /**
- * 
+ *
  * @param relativeLocFromDist relatived file location compared to ${dist folder}
  * @returns Promise<string>
- * 
+ *
  * touch a local file, and resolved content out
  */
-function getLocalSourceContent(relativeLocFromDist: string) {
+export function getLocalSourceContent(relativeLocFromDist: string) {
   const absPath = path.join(COMMON.distDir, relativeLocFromDist);
   if (fs.statSync(absPath) && fs.statSync(absPath).isFile()) {
     return new Promise((rsl, rej) => {
@@ -57,74 +59,83 @@ function getLocalSourceContent(relativeLocFromDist: string) {
 
 
 /**
- * 
+ *
  * @param elementString a stringified dom node content
+ * @param warnningLevel
  * @returns string
- * 
+ *
  * extract src/href attribute from a stringified dom node content
- * 
- * @samples 
+ *
+ * @samples
  * extractTargetPath('<script src="http://www.gov.cn" />'); // 'http://www.gov.cn'
  */
-function extractTargetPath(elementString: string) {
-  return elementString
-    .match(/(src|href)\=(\"|\').+(\"|\')/g)[0]
-    .match(/\".+\"/g)[0]
-    .replace(/\"/g, '')
-    .replace(COMMON.baseUrl, '');
+export function extractSourceLocation(elementString: string, warnningLevel?: IPluginProps['nonMatched']) {
+  const firstMatching = elementString!.match(/(src|href)\=(\"|\').+(\"|\')/g)?.[0];
+  if (!firstMatching) {
+    //
+    const errorMsg = `rollup-plugin-inline-source can not match any sources by src/href attribute specified to ${firstMatching}, is that correct?`;
+    if (warnningLevel === 2 || warnningLevel === 'error') {
+      throw new Error(errorMsg);
+    }
+    if (warnningLevel === 1 || warnningLevel === 'warn') {
+      chalk.yellow(errorMsg);
+    }
+  } else {
+    return firstMatching.match(/\".+\"/g)?.[0]?.replace(/\"/g, '').replace(COMMON.baseUrl, '');
+  }
 }
 
 
 /**
- * 
+ *
  * @param elementString a stringified dom node content
  * @returns string
- * 
+ *
  * extract tag name from a stringified dom node content
- * 
+ *
  * @samples
  * extractTagName('<script src="http://www.gov.cn" />'); // 'script'
  * extractTagName('<link href="http://www.gov.cn" />'); // 'style'
- * 
+ *
  */
-function extractTagName(elementString: string) {
+export function extractExpectedTagName(elementString: string) {
   const result = elementString.substring(1, elementString.indexOf(' '));
   return result === 'link' ? 'style' : result;
 }
 
 
 /**
- * 
+ *
  * @param elementString a stringified dom node content
  * @returns Array<string>
- * 
+ *
  * extract attributes from a stringified dom node content
- * 
+ *
  * @samples
- * extractProperties('<div class="cols-4 bg-gray" data-winner="RUSSIA" />'); // ['class="cols-4"', 'data-winner="RUSSIA"']
- * 
+ * extractProperties('<div class="cols-4 bg-gray" data-winner="RUSSIA" class="" id="testet" >123</div>'); // [ ' class="cols-4 bg-gray" data-winner="RUSSIA" class="" id="testet"' ]
+ *
  */
-function extractProperties(elementString: string) {
-  const segments = elementString.split('></')[0].split(' ');
-  return segments.filter((sg) => sg !== `<${extractTagName(elementString)}` && sg !== '<link' && !sg.includes('src=') && !sg.includes('href=') && !sg.includes('/>'));
+export function extractPropertiesString(elementString: string) {
+  const result: (string[] | null) = elementString.match(/\s?(\w|\-|\_)+(\=(\".*\"))+/g);
+  return result ? result[0] : ' '
 }
 
 
 /**
- * 
- * @param htmlNodeString 
- * @param determiningUnmountProperties 
- * @returns 
+ *
+ * @param elementString  a stringified dom node content
+ * @param determiningUnmountProperties the props who cause this plugin skip its transformation while they were detected
+ * @returns boolean
  */
-function checkShouldMount(htmlNodeString, determiningUnmountProperties = ['async', 'defer', { key: 'type', value: 'module' }, { key: 'rel', value: 'modulepreload' }]) {
+export function checkShouldMount(elementString: string, determiningUnmountProperties = ['async', 'defer', { key: 'type', value: 'module' }, { key: 'rel', value: 'modulepreload' }]) {
   // 判断是否有src/href
-  if (!/(src|href)\=(\"|\').+(\"|\')/g.test(htmlNodeString)) {
+  if (!/(src|href)\=(\"|\').+(\"|\')/g.test(elementString)) {
     return false;
   }
   // 如果有，则判断是否在dist文件中有或为链接
-  const fileTarget = extractTargetPath(htmlNodeString);
+  const fileTarget = extractSourceLocation(elementString) || '';
   const absPath = path.join(COMMON.distDir, fileTarget);
-  if (!fs.existsSync(absPath) && !/(http|https|):\/\//g.test(fileTarget) && fileTarget !== '//at.alicdn.com/t/font_2857489_imieufzfwmb.css') {
+  if (!fs.existsSync(absPath) && !/(http|https|):\/\//g.test(fileTarget)) {
     return false;
   }
 
@@ -132,10 +143,10 @@ function checkShouldMount(htmlNodeString, determiningUnmountProperties = ['async
   if (
     determiningUnmountProperties.some(function (prop) {
       if (typeof prop === 'string') {
-        return new RegExp(`${prop}`, 'g').test(htmlNodeString);
+        return new RegExp(`${prop}`, 'g').test(elementString);
       } else {
         const { key, value } = prop;
-        return new RegExp(`${key}\=\"${value}\"`, 'g').test(htmlNodeString);
+        return new RegExp(`${key}\=\"${value}\"`, 'g').test(elementString);
       }
     })
   ) {
@@ -145,39 +156,16 @@ function checkShouldMount(htmlNodeString, determiningUnmountProperties = ['async
   return true;
 }
 
+
 /**
- * Main
+ *
+ * @param content
+ * @param chalkInstance
+ * @param withBreaklines
+ * @returns void
  */
-const scanFiles = ['index.html'];
-const terserOption = {
-  removeComments: true,
-  collapseWhitespace: true,
-  removeEmptyAttributes: true,
-};
-//
-const scannedFileContent = getLocalFileSourceContent(scanFiles[0]);
-const scriptNodeStrings = scannedFileContent.match(/\<script.+\>\<\/script\>/g);
-const linkNodeStrings = scannedFileContent.match(/\<link.+\/?>/g);
-const needMountNodes = [...scriptNodeStrings, ...linkNodeStrings].filter(function (nodeContent) {
-  return checkShouldMount(nodeContent, ['async', 'defer', { key: 'rel', value: 'modulepreload' }]);
-});
-let result = scannedFileContent;
-console.log('----- COMMON.baseUrl is: ' + COMMON.baseUrl);
-console.log('Mounting resources are: ');
-console.table(needMountNodes);
-needMountNodes.forEach(async (nodeString) => {
-  const absPath = path.join(COMMON.distDir, extractTargetPath(nodeString));
-  if (fs.existsSync(absPath) || fs.existsSync(absPath.replace(COMMON.baseUrl, ''))) {
-    new RegExp(nodeString, 'g').test(result) && console.log('⛵ Located ' + nodeString, ', whoes resource at ' + absPath);
-    // 在dist里
-    result = result.replace(new RegExp(nodeString, 'g'), `<${extractTagName(nodeString)} ${extractProperties(nodeString).join(' ')}>${fs.readFileSync(absPath)}</${extractTagName(nodeString)}>`);
-    console.log('🚗 Mounting ' + nodeString, ', to inline resource, with properties settled as: ' + extractProperties(nodeString).join(' and '));
-  } else {
-    // 是远程的
-    new RegExp(nodeString, 'g').test(result) && console.log('⛵ Remote requested ' + nodeString, 'at URL: ' + extractTargetPath(nodeString));
-    const requestedContent = await getRemoteSourceContent(extractTargetPath(nodeString));
-    const content = await minify(requestedContent, terserOption);
-    result = result.replace(new RegExp(nodeString, 'g'), `<${extractTagName(nodeString)} ${extractProperties(nodeString).join(' ')}>${content}</${extractTagName(nodeString)}>`);
-    console.log('🚗 Mounting ' + nodeString, ', to inline resource, with properties settled as: ' + extractProperties(nodeString).join(' and '));
-  }
-});
+export function chalkSay(content: string, chalkInstance = chalk.green, withBreaklines = true) {
+  withBreaklines && console.log(' ');
+  console.log(chalkInstance(content));
+  withBreaklines && console.log(' ');
+}
